a7f956fa75ab72398fa693fbdb9ebd374cbd6dc4
montotar <monton_to@hotmail.com>
2021-06-09 11:54:18 +0700

    dd

--- pages/permission_doc.php
+++ pages/permission_doc.php
@@ -26,7 +26,7 @@
 
             <div class="row">
               <div class="col-4">
-                <div class="row mt-2  table-responsive p-0" id="tb_Data" style="height: 500px;max-height: 500px;overflow-y: auto;">
+                <div class="row mt-5 " id="tb_Data" style="height: 500px;max-height: 500px;overflow-y: auto;">
                   <div class="col-12">
                     <table id="Data_TableLeft" class="table table-bordered table-hover w-100 table-head-fixed">
                       <thead>
@@ -45,17 +45,17 @@
             
               <div class="col-8">
 
-              <div class="col-3">
-                <input type="text" class="form-control" id="txtSearch" onkeyup="showData_Doc();" placeholder="ค้นหารายการ">
-              </div>
+                <div class="col-3">
+                  <input type="text" class="form-control" id="txtSearch" onkeyup="showData_Doc();" placeholder="ค้นหารายการ">
+                </div>
 
                 <div class="row mt-2 card-body table-responsive p-0" id="tb_Data2" style="height: 500px;max-height: 500px;overflow-y: auto;">
                   <div class="col-12">
                     <table id="Data_TableRight" class="table table-bordered table-hover w-100 table-head-fixed">
                       <thead>
                         <tr class="text-center">
-                          <th style="width: 20%;" class="bg_tableAll">เลือก</th>
-                          <th style="width: 80%;" class="bg_tableAll">เอกสาร</th>
+                          <th style="width: 10%;" class="bg_tableAll">เลือก</th>
+                          <th style="width: 90%;" class="bg_tableAll">เอกสาร</th>
                         </tr>
                       </thead>
                       <tbody>
